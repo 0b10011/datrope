93680a98c8d3080a33065a104af2e79abc05daf1
Brandon Frohs <brandon@19.codes>
2024-05-25 04:13:58 -0400

    expand guild scheduled events objects

--- src/resources/guild_scheduled_event.rs
+++ src/resources/guild_scheduled_event.rs
@@ -1,19 +1,66 @@
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
+#[cfg(feature = "serde")]
+use time::serde::iso8601;
+use time::OffsetDateTime;
+
+use super::{
+    auto_moderation::CreatorId,
+    channel::ChannelId,
+    guild::{GuildId, GuildMember},
+    user::User,
+    ImageHash,
+};
 
 /// Discord docs: https://discord.com/developers/docs/resources/guild-scheduled-event#guild-scheduled-event-object
 #[cfg_attr(feature = "clone", derive(Clone))]
 #[cfg_attr(feature = "debug", derive(Debug))]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
-pub struct GuildScheduledEvent {}
+pub struct GuildScheduledEvent {
+    pub id: GuildScheduledEventId,
+    pub guild_id: GuildId,
+    pub channel_id: Option<ChannelId>,
+    #[cfg_attr(feature = "serde", serde(default))]
+    pub creator_id: Option<CreatorId>,
+    pub name: String,
+    #[cfg_attr(feature = "serde", serde(default))]
+    pub description: Option<String>,
+    #[cfg_attr(feature = "serde", serde(with = "iso8601"))]
+    pub scheduled_start_time: OffsetDateTime,
+    #[cfg_attr(feature = "serde", serde(default))]
+    #[cfg_attr(feature = "serde", serde(with = "iso8601::option"))]
+    pub scheduled_end_time: Option<OffsetDateTime>,
+    pub privacy_level: GuildeScheduledEventPrivacyLevel,
+    pub status: GuildScheduledEventStatus,
+    pub entity_type: GuildScheduledEventEntityType,
+    pub entity_id: Option<EntityId>,
+    pub entity_metadata: Option<GuildScheduledEventEntityMetadata>,
+    #[cfg_attr(feature = "serde", serde(default))]
+    pub creator: Option<User>,
+    #[cfg_attr(feature = "serde", serde(default))]
+    pub user_count: Option<u64>,
+    #[cfg_attr(feature = "serde", serde(default))]
+    pub image: Option<ImageHash>,
+}
+
+#[cfg_attr(feature = "clone", derive(Clone))]
+#[cfg_attr(feature = "debug", derive(Debug))]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+pub struct GuildScheduledEventId(pub String);
 
 /// Discord docs: https://discord.com/developers/docs/resources/guild-scheduled-event#guild-scheduled-event-object-guild-scheduled-event-privacy-level
 #[cfg_attr(feature = "clone", derive(Clone))]
 #[cfg_attr(feature = "debug", derive(Debug))]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
-pub enum GuildScheduledEventPrivacyLevel {
+#[cfg_attr(feature = "serde", serde(rename_all = "SCREAMING_SNAKE_CASE"))]
+pub enum GuildeScheduledEventPrivacyLevel {
     GuildOnly = 2,
 }
+
+#[cfg_attr(feature = "clone", derive(Clone))]
+#[cfg_attr(feature = "debug", derive(Debug))]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+pub struct EntityId(pub String);
 
 /// Discord docs: https://discord.com/developers/docs/resources/guild-scheduled-event#guild-scheduled-event-object-guild-scheduled-event-entity-types
 #[cfg_attr(feature = "clone", derive(Clone))]
@@ -40,10 +87,18 @@ pub enum GuildScheduledEventStatus {
 #[cfg_attr(feature = "clone", derive(Clone))]
 #[cfg_attr(feature = "debug", derive(Debug))]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
-pub struct GuildScheduledEventEntityMetadata {}
+pub struct GuildScheduledEventEntityMetadata {
+    #[cfg_attr(feature = "serde", serde(default))]
+    pub location: Option<String>,
+}
 
 /// Discord docs: https://discord.com/developers/docs/resources/guild-scheduled-event#guild-scheduled-event-user-object
 #[cfg_attr(feature = "clone", derive(Clone))]
 #[cfg_attr(feature = "debug", derive(Debug))]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
-pub struct GuildScheduledEventUser {}
+pub struct GuildScheduledEventUser {
+    pub guild_scheduled_event_id: GuildScheduledEventId,
+    pub user: User,
+    #[cfg_attr(feature = "serde", serde(default))]
+    pub member: Option<GuildMember>,
+}
